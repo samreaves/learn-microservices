c752ed31f7d3551d9baba73d21f5a8882c9239de
Sam <thesamreaves@gmail.com>
2020-05-05 16:30:42 -0400

    Removed signup route from constants as I solved test auth a different way

--- tickets/src/constants/routes.ts
+++ tickets/src/constants/routes.ts
@@ -1,4 +1,3 @@
-export const SIGNUP_ROUTE = '/api/auth/signup';
 export const GET_TICKETS_ROUTE = '/api/tickets';
 export const GET_TICKET_BY_ID_ROUTE = '/api/tickets:id';
 export const CREATE_TICKET_ROUTE = '/api/tickets';
